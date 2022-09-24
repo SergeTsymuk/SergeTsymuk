bd4a53d9df3a3a189cc9625527a188237f3f67f8
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-09-24 16:12:51 -0400

    DelagatesProject

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    public delegate void NoParams();
+    public delegate string WithParams(string name1, string name2);
+    static void Main(string[] args)
+    {
+        NoParams noPrm = new(ShowMessage);
+        noPrm();
+
+        WithParams withPrm = new(ShowMessage);
+        string value = withPrm("Me", "You");
+        Console.WriteLine(value);
+        Console.ReadLine();
+    }
+    private static void ShowMessage()
+    {
+        Console.WriteLine("Have Fun");
+    }
+
+    private static string ShowMessage(string name1, string name2)
+    {
+        Console.WriteLine($"{name1}, {name2} Have fun! ");
+        return "with C#!";
+    }
+}
