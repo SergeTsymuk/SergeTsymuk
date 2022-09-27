331b81daebb6304654ccbfb78ab8629b4ab83990
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-09-27 13:22:26 -0400

    Created DB and connected usin Entity Framework

---
+++ ASP_DB_Connected/ASP_DB_Connected/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ASP_DB_Connected.Data;
+
+namespace ASP_DB_Connected.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ASP_DB_Connected.Data.Employee> Employee { get; set; }
+    }
+}
