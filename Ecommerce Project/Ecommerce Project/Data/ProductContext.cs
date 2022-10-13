148e91c7ad9eaa4194a54d03e6130a37f25bce8f
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-10-13 11:29:29 -0400

    DbContext Created

---
+++ Ecommerce Project/Ecommerce Project/Data/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_Project.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
