4e8470cc258bbc786e3eaa7542454cb731555948
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-10-14 10:39:18 -0400

    Migration added

---
+++ Ecommerce Project/Ecommerce Project/DataAccess/PagesContext.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_Project.DataAccess
+{
+    public class PagesContext : DbContext 
+    {
+        public PagesContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Page> Pages { get; set; }
+    }
+}
---
+++ Ecommerce Project/Ecommerce Project/Migrations/20221014013904_InitDbCreation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDbCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Slug = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sorting = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pages");
+        }
+    }
+}
