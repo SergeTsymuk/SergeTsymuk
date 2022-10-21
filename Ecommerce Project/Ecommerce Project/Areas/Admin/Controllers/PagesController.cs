e1f1a1922456b2db50ed41e5e3b138da85487324
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-10-21 11:39:57 -0400

    Page Controllee Added

---
+++ Ecommerce Project/Ecommerce Project/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce_Project.DataAccess;
+using Ecommerce_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly PagesContext context;
+
+        public PagesController(PagesContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+            return View(pagesList);
+        }
+    }
+}
